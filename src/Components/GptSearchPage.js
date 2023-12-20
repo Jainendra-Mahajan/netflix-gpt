de30c9a0054d492d795db4316753cee636cbfc0f
Jainendra <jainendra.mahajan456@gmail.com>
2023-12-20 17:13:58 +0530

    Multiple Language Option In GPT search

--- src/Components/GptSearchPage.js
+++ src/Components/GptSearchPage.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import lang from "../utils/languageConstants"
+import { useSelector } from 'react-redux'
 
 const GptSearchPage = () => {
+
+    const currentLanguage = useSelector(store => store.lang.currentLanguage);
     return (
         <div className='pt-[10%] flex justify-center'>
             <form className='w-1/2 bg-black grid grid-cols-12'>
                 <input className='p-4 m-4 rounded-lg col-span-9'
-                    type="text" placeholder='What would you like to watch Today ?' />
-                <button className='py-2 px-4 m-4 bg-red-600 text-white rounded-lg col-span-3'>Search</button>
+                    type="text" placeholder={lang[currentLanguage].gptSearchPlaceHolder} />
+                <button className='py-2 px-4 m-4 bg-red-600 text-white rounded-lg col-span-3'>{lang[currentLanguage].search}</button>
             </form>
         </div>
     )
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,14 +4,16 @@ import { auth } from '../utils/firebase';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../utils/userSlice';
-import { LOGO, USER_LOGO } from '../utils/constants';
+import { LOGO, SUPPORTED_LANGUAGES, USER_LOGO } from '../utils/constants';
 import { addGptButtonValue } from '../utils/gptSlice';
+import { changeLanguage } from '../utils/configSlice';
 
 const Header = () => {
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user); // subscribing the store and fetching the details.
+  const gptValue = useSelector(store => store.gpt.gptState);
   const handleSignOut = () => {
 
     signOut(auth).then(() => {
@@ -39,6 +41,11 @@ const Header = () => {
   const hadleGptsearchClick = () => {
     dispatch(addGptButtonValue());
   }
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  }
+
   return (
     <div className='w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-50 flex justify-between'>
       <img className='w-48'
@@ -46,9 +53,16 @@ const Header = () => {
         alt="logo" />
 
       {user && <div className='flex p-4'>
+
+        {gptValue && <select onChange={handleLanguageChange}
+          className='p-2 m-2 bg-gray-900 text-white rounded-lg'>
+          {SUPPORTED_LANGUAGES.map(lang => <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
+        </select>}
+
         <button onClick={hadleGptsearchClick}
-          className='px-2 bg-purple-700 text-white rounded-lg mr-6 mb-2'>
-          Gpt Search</button>
+          className='px-3 bg-purple-700 text-white rounded-lg mr-6 mb-2'>
+          {gptValue ? "Home" : "Gpt Search"}
+        </button>
         <img
           className='w-10 h-10'
           src={USER_LOGO} alt="User Icon" />
