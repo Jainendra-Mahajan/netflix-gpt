957d86ecccf3f0d4d85f3812fa1babfbadb4575f
Jainendra <jainendra.mahajan456@gmail.com>
2024-01-05 17:04:48 +0530

    Movie Card Changes

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -5,8 +5,8 @@ const MovieCard = ({ poster }) => {
 
     if (!poster) return null;
     return (
-        <div className='w-24 md:w-36 pr-4 pt-3'>
-            <img src={IMG_CDN_URL + poster} alt="Movie Card" />
+        <div className='w-36 md:w-48 pr-4 pt-3 md:h-72'>
+            <img src={IMG_CDN_URL + poster} alt="Movie Card" className='rounded-lg' />
         </div>
     )
 }
